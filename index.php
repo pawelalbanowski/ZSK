adc03c2ad5a1e95c8ba1b75ae2a734cf3add4003
pawelalbanowski <pawel.albanowski@gmail.com>
2019-09-17 10:41:20 +0200

    index22

--- index.php
+++ index.php
@@ -5,6 +5,43 @@
     <title></title>
   </head>
   <body>
+    <?php
+      $liczba = 10;
+      $_liczba = 10;
+      $imię = 'Janusz';
 
+
+      $prawda = true;
+
+      //typ integer
+      $calkowita = 10;
+      $hex = 0xA;
+
+      echo $hex;
+
+      $oct = 010;
+
+      echo $oct;
+
+      $bin = 0b1010;
+
+      echo $bin;
+//sposoby wyświetlania
+      echo $hex,$bin,$oct; //po kolei
+      echo $hex.$bin.$oct; //łączy stringi i wyświetla
+
+echo '<hr>',$bin,'<hr>';
+
+      //składnia heredoc
+
+      $imie = 'Jan';
+      $nazwisko = 'Kowalski';
+
+      $text = <<< TEKST
+      Twoje imię: $imie
+      TEKST;
+
+      echo $text;
+     ?>
   </body>
 </html>
